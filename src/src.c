83b4349558797e87c3b6570d9f14fca7a597df4b
Dinh Viet Anh <jokerva1010123@gmail.com>
2024-01-02 09:37:22 +0300

    Update

--- src/src.c
+++ src/src.c
@@ -1,21 +1,53 @@
-#include <linux/init.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("Keyboard LED control module");
-
-static int __init inita(void)
+#define KBD_DATA_REG        0x60    /* I/O port for keyboard data */
+#define KBD_CONTROL_REG        0x64 
+#define KBD_SCANCODE_MASK   0x7f
+#define KBD_STATUS_MASK     0x80
+#define DELAY do { mdelay(1); if (++delay > 10) break; } while(0)
+static void set_kbd_led_on(void)
 {
-    printk("inita\n");
-    return 0;
+	long delay = 0;
+	//wait till the input buffer is empty
+	while (inb(KBD_CONTROL_REG) & 0x02)
+		DELAY;
+	outb(0xED, KBD_DATA_REG);
+	DELAY;
+	while (inb(KBD_CONTROL_REG) & 0x02)
+		DELAY;
+	outb(0x07, KBD_DATA_REG);
+	DELAY;
+}
+static void set_kbd_led_off(void)
+{
+	long delay = 0;
+	//wait till the input buffer is empty
+	while (inb(KBD_CONTROL_REG) & 0x02)
+		DELAY;
+	outb(0xED, KBD_DATA_REG);
+	DELAY;
+	while (inb(KBD_CONTROL_REG) & 0x02)
+		DELAY;
+	outb(0x00, KBD_DATA_REG);
+	DELAY;
 }
 
-static void __exit exita(void)
+static int test_kbd_led_init(void)
 {
-    printk("exita\n");
+        pr_info("%s: In init\n", __func__);
+	set_kbd_led_on();
+	return 0;
 }
 
-module_init(inita);
-module_exit(exita);
+static void test_kbd_led_exit(void)
+{
+        pr_info("%s: In exit\n", __func__);
+	set_kbd_led_off();
+}
+
+module_init(test_kbd_led_init);
+module_exit(test_kbd_led_exit);
